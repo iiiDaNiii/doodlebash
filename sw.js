c8cd020f40cc00ff65dcf14ae4db248e66080a79
iiiDaNiii <31015434+iiiDaNiii@users.noreply.github.com>
2026-05-21 09:40:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'doodle-bash-v3.17';   // ← Increase this number (v2, v3…) every time you update the app
+const CACHE_NAME = 'doodle-bash-v3.18';   // ← Increase this number (v2, v3…) every time you update the app
 
 self.addEventListener('install', event => {
   event.waitUntil(
