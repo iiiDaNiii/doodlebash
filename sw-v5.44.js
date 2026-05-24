6c29eeb5ca516f14626d4ff469b142ce56e86792
iiiDaNiii <31015434+iiiDaNiii@users.noreply.github.com>
2026-05-23 21:39:30 -0300

    Update and rename sw-v5.43.js to sw-v5.44.js

--- sw-v5.44.js
+++ sw-v5.44.js
@@ -1,5 +1,5 @@
 // ====================== DOODLE BASH SERVICE WORKER — DIAGNOSTIC ======================
-const CACHE_NAME = 'doodle-bash-v5.43';   // ← BUMP THIS EVERY TEST
+const CACHE_NAME = 'doodle-bash-v5.44';   // ← BUMP THIS EVERY TEST
 
 const PRECACHE_ASSETS = [
   '/',
