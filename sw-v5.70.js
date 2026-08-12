81069a81349069bb7838a2483632c19c993335cf
iiiDaNiii <31015434+iiiDaNiii@users.noreply.github.com>
2026-08-11 22:25:57 -0300

    Update and rename sw-v5.69.js to sw-v5.70.js

--- sw-v5.70.js
+++ sw-v5.70.js
@@ -1,5 +1,5 @@
 // ====================== DOODLE BASH SERVICE WORKER — DIAGNOSTIC ======================
-const CACHE_NAME = 'doodle-bash-v5.69';   // ← BUMP THIS EVERY TEST
+const CACHE_NAME = 'doodle-bash-v5.70';   // ← BUMP THIS EVERY TEST
 
 const PRECACHE_ASSETS = [
   '/',
